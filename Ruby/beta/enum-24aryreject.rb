5bf8610df598a318272b884f61a6032fa680536c
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-08 15:33:50 +0000

    add enum24

---
+++ Ruby/beta/enum-24aryreject.rb
@@ -0,0 +1,15 @@
+=begin
+
+Description:
+
+Create a method reject that accepts a list and a block, and returns a list of elements for which the block returns false.
+
+If you need help, here's a reference:
+
+http://www.rubycuts.com/enum-reject
+
+=end
+
+def reject list, &block
+  list.reject(&block)
+end
