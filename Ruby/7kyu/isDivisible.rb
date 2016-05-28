7332141f24ac84859a3958d23eaf7a5a689370b1
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-28 10:17:28 +0100

    minor changes

--- Ruby/7kyu/isDivisible.rb
+++ Ruby/7kyu/isDivisible.rb
@@ -1,7 +1,7 @@
 =begin
 Create a function isDivisible(n,...) that checks if the first agrument n is divisible by all other arguments
 (return true if no other arguments)
-
+...
 =end
 
 def is_divisible(n,*args)
