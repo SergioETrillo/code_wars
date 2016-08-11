517a1de43d785e9772844d90d23b856769361653
tigretoncio <senrech@yahoo.fr>
2016-08-11 14:35:51 +0100

    first test

---
+++ C#/6kyu/PrizeDraw/PriceDraw/PriceDrawTests/ProgramTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PriceDraw;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceDraw.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void test1()
+        {
+            string st = "";
+            int[] we = new int[] { 4, 2, 1, 4, 3, 1, 2 };
+            Assert.AreEqual("No participants", Rank.NthRank(st, we, 4));
+            //st = "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin";
+
+        }
+
+
+    }
+}
